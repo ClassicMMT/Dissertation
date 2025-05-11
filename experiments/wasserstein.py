56c8ba79734c4f35c5dc9d8b5155857f464317b1
ClassicMMT <porlovskiy@hotmail.com>
2025-05-11 21:21:59 +1200

    class imbalance & successful-attacks only experiments

--- experiments/wasserstein.py
+++ experiments/wasserstein.py
@@ -22,9 +22,6 @@ from utils import (
 
 np.random.seed(123)
 
-import numpy as np
-
-np.vstack()
 
 ##### Same scale distributions
 
@@ -90,14 +87,14 @@ ss(x, adversarial_overfit).mean()
 ### By class experiment
 print(
     "Blue class",
-    "\nNormal Accuracy",
+    "\nNormal distance",
     wasserstein_distance_nd(x[y == 0, :], adversarial_normal[y == 0, :]),
-    "\nOverfit Accuracy",
+    "\nOverfit distance",
     wasserstein_distance_nd(x[y == 0, :], adversarial_overfit[y == 0, :]),
     "\nOrange class",
-    "\nNormal Accuracy",
+    "\nNormal distance",
     wasserstein_distance_nd(x[y == 1, :], adversarial_normal[y == 1, :]),
-    "\nOverfit Accuracy",
+    "\nOverfit distance",
     wasserstein_distance_nd(x[y == 1, :], adversarial_overfit[y == 1, :]),
 )
 
@@ -129,8 +126,8 @@ x2 = np.vstack(
 x = np.hstack((x1, x2))
 y = np.hstack((np.zeros(100), np.ones(100))).astype(int)
 
-# plt.scatter(x[:, 0], x[:, 1], c=y)
-# plt.show()
+plt.scatter(x[:, 0], x[:, 1], c=y)
+plt.show()
 
 normal = SVC(
     kernel="rbf",
@@ -186,6 +183,87 @@ print(  # by class
 
 ##### Class imbalance experiment
 
+np.random.seed(412)
+x1 = np.random.normal(loc=0, scale=5, size=(200, 1))
+x2 = np.vstack(
+    (
+        np.random.normal(loc=5, scale=1, size=(100, 1)),
+        np.random.normal(loc=0, scale=1, size=(100, 1)),
+    )
+)
+x = np.hstack((x1, x2))
+y = np.hstack((np.zeros(100), np.ones(100))).astype(int)
+
+# create imbalance by removing 1/2 of class 1
+
+indices = np.random.choice(100, 50)
+x = np.vstack((x[indices], x[100:]))
+y = np.hstack((y[indices], y[100:]))
+np.unique(y, return_counts=True)
+
+normal = SVC(
+    kernel="rbf",
+    random_state=111,
+)
+
+overfit = SVC(
+    kernel="rbf",
+    random_state=112,
+    gamma=50,
+)
+
+normal.fit(x, y)
+overfit.fit(x, y)
+
+(get_accuracy(normal.predict(x), y), get_accuracy(normal.predict(x), y))
+
+
+# plot_svm(normal, x, y, title="normal")
+# plot_svm(overfit, x, y, title="overfit")
+
+adversarial_normal = HopSkipJump(SklearnClassifier(model=normal)).generate(x)
+adversarial_overfit = HopSkipJump(SklearnClassifier(model=overfit)).generate(x)
+
+adversarial_y_normal = normal.predict(adversarial_normal)
+adversarial_y_overfit = overfit.predict(adversarial_overfit)
+
+print(
+    "Predictions on adv points",
+    "\nNormal Accuracy",
+    np.mean(adversarial_y_normal == y).item(),
+    "\nOverfit Accuracy:",
+    np.mean(adversarial_y_overfit == y).item(),
+)
+
+plot_attacks_svm(x, y, adversarial_normal, adversarial_overfit, normal, overfit)
+plot_attacks(x, y, adversarial_normal, adversarial_overfit, normal, overfit)
+
+
+wasserstein_distance_nd(x, adversarial_normal)
+wasserstein_distance_nd(x, adversarial_overfit)
+
+ss(x, adversarial_normal).mean()
+ss(x, adversarial_overfit).mean()
+
+### By class
+print(
+    "Blue class",
+    "\nNormal Accuracy",
+    wasserstein_distance_nd(x[y == 0, :], adversarial_normal[y == 0, :]),
+    "\nOverfit Accuracy",
+    wasserstein_distance_nd(x[y == 0, :], adversarial_overfit[y == 0, :]),
+    "\nOrange class",
+    "\nNormal Accuracy",
+    wasserstein_distance_nd(x[y == 1, :], adversarial_normal[y == 1, :]),
+    "\nOverfit Accuracy",
+    wasserstein_distance_nd(x[y == 1, :], adversarial_overfit[y == 1, :]),
+)
+
+ss(x[y == 0, :], adversarial_normal[y == 0, :]).mean()
+ss(x[y == 1, :], adversarial_normal[y == 1, :]).mean()
+ss(x[y == 1, :], adversarial_overfit[y == 0, :]).mean()
+ss(x[y == 1, :], adversarial_overfit[y == 1, :]).mean()
+
 
 ##### Linear separability experiment
 
@@ -199,9 +277,101 @@ print(  # by class
 
 ##### Wasserstein distance but only for successful adversarial points experiment
 
+np.random.seed(124)  # changed from 125 to 124 to match different-scale experiment
+
+x1 = np.vstack(
+    (
+        np.random.normal(loc=0, scale=10, size=(100, 1)),
+        np.random.normal(loc=0, scale=1, size=(100, 1)),
+    )
+)
+x2 = np.vstack(
+    (
+        np.random.normal(loc=5, scale=1, size=(100, 1)),
+        np.random.normal(loc=0, scale=1, size=(100, 1)),
+    )
+)
+x = np.hstack((x1, x2))
+y = np.hstack((np.zeros(100), np.ones(100))).astype(int)
+
+# plt.scatter(x[:, 0], x[:, 1], c=y)
+# plt.show()
+
+normal = SVC(
+    kernel="rbf",
+    random_state=103,
+)
+
+overfit = SVC(
+    kernel="rbf",
+    random_state=104,
+    gamma=50,
+)
+
+normal.fit(x, y)
+overfit.fit(x, y)
+
+preds_normal = normal.predict(x)
+preds_overfit = overfit.predict(x)
+(get_accuracy(normal.predict(x), y), get_accuracy(normal.predict(x), y))
+
+adversarial_normal = HopSkipJump(SklearnClassifier(model=normal)).generate(x)
+adversarial_overfit = HopSkipJump(SklearnClassifier(model=overfit)).generate(x)
+
+adversarial_y_normal = normal.predict(adversarial_normal)
+adversarial_y_overfit = overfit.predict(adversarial_overfit)
+
+print(
+    "Predictions on adv points - attack failure rate",
+    "\nNormal Accuracy",
+    np.mean(adversarial_y_normal == y).item(),
+    "\nOverfit Accuracy:",
+    np.mean(adversarial_y_overfit == y).item(),
+)
+
+plot_attacks_svm(x, y, adversarial_normal, adversarial_overfit, normal, overfit)
+plot_attacks(x, y, adversarial_normal, adversarial_overfit, normal, overfit)
+
+
+# Attacks which were successful but only where the model got them correct in the first place
+i_normal = (adversarial_y_normal != y) & (y == preds_normal)
+i_overfit = (adversarial_y_overfit != y) & (y == preds_overfit)
+
+wasserstein_distance_nd(x[i_normal, :], adversarial_normal[i_normal, :])
+wasserstein_distance_nd(x[i_overfit, :], adversarial_overfit[i_overfit, :])
+
+# separated by class
+print(
+    "Blue class",
+    "\nNormal Accuracy",
+    wasserstein_distance_nd(
+        x[i_normal & y == 0, :],
+        adversarial_normal[i_normal & y == 0, :],
+    ),
+    "\nOverfit Accuracy",
+    wasserstein_distance_nd(
+        x[i_overfit & y == 0, :],
+        adversarial_overfit[i_overfit & y == 0, :],
+    ),
+    "\nOrange class",
+    "\nNormal Accuracy",
+    wasserstein_distance_nd(
+        x[i_normal & y == 1, :],
+        adversarial_normal[i_normal & y == 1, :],
+    ),
+    "\nOverfit Accuracy",
+    wasserstein_distance_nd(
+        x[i_overfit & y == 1, :],
+        adversarial_overfit[i_overfit & y == 1, :],
+    ),
+)
+
 
 ##### Impact of sample size on wasserstein distance experiment
 
+
+##### Impact of "level of overfitting" experiment
+"""Just change gamma and explore what happens to the wasserstein distance when we have more/less overfitting"""
 
 ##### Other attacks
 
