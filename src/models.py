61db3fcf19e91df067cabfa77489db3fe764848c
ClassicMMT <porlovskiy@hotmail.com>
2025-09-03 16:28:59 +1200

    add heloc

--- src/models.py
+++ src/models.py
@@ -3,6 +3,22 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+class HelocNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(23, 128)
+        self.fc2 = nn.Linear(128, 32)
+        self.fc3 = nn.Linear(32, 2)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
+        return x
+
+
 class SpamBaseNet(nn.Module):
     def __init__(self):
         super().__init__()
