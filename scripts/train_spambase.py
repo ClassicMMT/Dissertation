5a0d2ed8edb0122351df99b8588194da92df9327
ClassicMMT <porlovskiy@hotmail.com>
2025-10-18 14:55:04 +1300

    train final spambase model

--- scripts/train_spambase.py
+++ scripts/train_spambase.py
@@ -12,46 +12,46 @@ from src.utils import (
     evaluate_model,
     save_model,
 )
-from src.datasets import load_spambase
+from src.datasets import create_loaders, load_spambase, scale_datasets
 from src.models import SpamBaseNet
 
 
 # Set variables and random seeds
 random_state = 123
 device = torch.device("mps")
-set_all_seeds(random_state)
+g = set_all_seeds(random_state)
+batch_size = 128
 
 # Load data
-(train_loader, test_loader), (train_dataset, test_dataset) = load_spambase(random_state=random_state)
+(train_loader, calib_loader, test_loader), (train_dataset, calib_dataset, test_dataset) = load_spambase(
+    batch_size=batch_size, test_size=0.25, return_train_val_test=True, random_state=random_state
+)
 
-training_accuracies = []
-test_accuracies = []
 
-epochs = [6, 10, 60, 100, 200, 300, 400]
+# Model
+model = SpamBaseNet().to(device)
+n_epochs = 30
 
-for epoch in epochs:
-    model = SpamBaseNet()
+########### SET NAME HERE ###########
+save_name = "spambase_" + "final"
+#####################################
 
-    ########### SET NAME HERE ###########
-    save_name = "spambase_" + str(epoch)
-    #####################################
+# Initialise model
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-    # Initialise model
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.01)
+# Train model
+model, train_accuracy = train_model(
+    model,
+    loader=train_loader,
+    criterion=criterion,
+    optimizer=optimizer,
+    n_epochs=n_epochs,
+    return_accuracy=True,
+)
 
-    # Train model
-    model, train_accuracy = train_model(
-        model,
-        loader=train_loader,
-        criterion=criterion,
-        optimizer=optimizer,
-        n_epochs=epoch,
-        return_accuracy=True,
-    )
+# Evaluate model
+print(f"SpamBase Model Test Accuracy: {evaluate_model(model ,test_loader, device=device):.4f}")
 
-    # Evaluate model
-    print(f"SpamBase Model Test Accuracy: {evaluate_model(model ,test_loader, device=device):.4f}")
-
-    # Save model
-    save_model(model, save_name)
+# Save model
+save_model(model, save_name)
